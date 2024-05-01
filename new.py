1e9744d3e008b89b709f992794f010f95c34a8ad
AenStarAX <940136983@qq.com>
2024-05-01 20:59:13 +0800

    new post & fixed some bug

--- new.py
+++ new.py
@@ -13,5 +13,7 @@ parser.add_argument('title', help='Title for the post')
 
 args = parser.parse_args()
 
+id = len(os.listdir(POST_PATH)) + 1
+
 with open(POST_PATH + '/' + args.title + '.md', 'w', encoding='utf-8') as f:
-    f.write(f'title:{args.title}\nauthor:{AUTHOR}\ndate:{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n---')
+    f.write(f'id:{id}\ntitle:{args.title}\nauthor:{AUTHOR}\ndate:{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n---')
